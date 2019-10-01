d9269020007c51b4d27de141b8ec4f8279007ed6
Vladimir6332 <vladimir6332@gmail.com>
2019-10-02 01:48:47 +0300

    feat: complete solution task

--- src/index.js
+++ src/index.js
@@ -35,27 +35,35 @@ const MORSE_TABLE = {
   '---..': '8',
   '----.': '9',
   '-----': '0',
+  '*****': ' ',
 };
 
 function decode(expr) {
   let alphabetBinary = {
-    '10' : '.',
-    '11' : '-'
+    '10': '.',
+    '11': '-',
+    '**': '*',
   };
   let arrExpr = expr.split('');
   let arrLettersBinary = [];
+  let arrDecodedLetters  = [];
   let temp;
-  for (let i = arrExpr.length(); i > 0;) {
+
+  for (let i = arrExpr.length; i > 0;) {
     arrLettersBinary.push(arrExpr.splice(0, 10));
+    i = arrExpr.length;
   };
-  arrLettersBinary.reduce((acc, cur, index, arr) => {
-    if (index % 2 === 0) {
-      temp = arr[index] + arr[index + 1];
-      if (alphabetBinary[temp]) return acc.push(alphabetBinary[temp])
-    }
-    cur.map(())
-    return acc
-  }, []);
+  arrDecodedLetters = arrLettersBinary.map((cur, index, arr) => {
+    let arrLetter = cur.reduce((acc, cur, index, arr) => {
+      if (index % 2 === 0) {
+        temp = arr[index].concat(arr[index + 1]);
+        if (alphabetBinary[temp]) acc.push(alphabetBinary[temp]);
+      };
+      return acc;
+    }, []);
+    return MORSE_TABLE[arrLetter.join('')];    
+  });
+  return arrDecodedLetters.join('');
 }
 
 module.exports = {
